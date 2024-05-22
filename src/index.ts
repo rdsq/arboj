a40387d69de049e4712b2e19be28508934fc31c4
rdsq <62446963+rdsq@users.noreply.github.com>
2024-05-22 10:45:04 +0300

    yaclil options refactor

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,7 @@
 import { getHelpCommand, renderHelp } from "./help/help";
 import { parseCommand } from "./parser";
-import type { Command } from "./types/command";
 import type { ParsedCommand } from "./types/parsed";
-
-export type YaclilOptions = {
-    /**
-     * Yaclil treats the whole CLI as a command tree
-     * that has its subcommands, options, args.
-     * This is that command
-     */
-    rootCommand: Command,
-    /**
-     * Advanced options. You don't need them in most cases.
-     */
-    advanced?: {
-        /**
-         * Enable or disable `--help` and `-h` options in the app.
-         * `true` by default
-         */
-        helpOptions?: boolean,
-        /**
-         * Custom argv, mostly for testing.
-         * Don't forget to set first two args to something useless,
-         * because the app will delete them automatically
-         */
-        argv?: string[],
-        /**
-         * Allow args, that were not expected by this app's commands.
-         * `false` by default
-         */
-        allowUnexpectedArgs?: boolean,
-        /**
-         * Allow options that were not expected by this app's commands.
-         * `false` by default
-         */
-        allowUnexpectedOptions?: boolean,
-    }
-};
+import type { YaclilOptions } from "./types/init";
 
 /**
  * The YACLIL API
