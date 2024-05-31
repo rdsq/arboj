a74f7a8e0c1b2d26b3b8d72befd5882759af41a8
rdsq <62446963+rdsq@users.noreply.github.com>
2024-05-31 17:51:00 +0300

    colored option for tree graph

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.3.2 2024-05-31
+
+`treeGraph` can now represent commands without handlers as dimmed
+
 ## 1.3.1 2024-05-31
 
 There is now `rootCommand` in parsed commands
--- src/tree-graph.ts
+++ src/tree-graph.ts
@@ -10,7 +10,7 @@ const branchAndStraightChar = '┣ ';
  * @param margin Count of `┃ ` repeats
  * @returns The graph of this command's subcommands
  */
-function recursiveTree(command: Command, margin: number): string {
+function recursiveTree(command: Command, margin: number, colored: boolean): string {
     const marginString = straightChar.repeat(margin);
     let result: string[] = [];
     const subcommandsLength = (command.subcommands?.length ?? 0);
@@ -21,13 +21,19 @@ function recursiveTree(command: Command, margin: number): string {
         if (i === subcommandsLength - 1) {
             char = branchChar;
         }
+        // get command name
+        let commandName = command.name;
+        if (colored && command.handler === null) {
+            // dim it if it does not have a handler and coloring is enabled
+            commandName = `\x1b[2m${commandName}\x1b[0m`;
+        }
         // add it
         result.push(
-            marginString + char + subcommand.name
+            marginString + char + commandName
         );
         // if it has subcommands
         if (subcommand.subcommands && subcommand.subcommands.length > 0) {
-            result.push(recursiveTree(subcommand, margin + 1));
+            result.push(recursiveTree(subcommand, margin + 1, colored));
         }
     }
     return result.join("\n");
@@ -36,9 +42,10 @@ function recursiveTree(command: Command, margin: number): string {
 /**
  * Generate tree graph from any command
  * @param command The command to create graph from
+ * @param colored Should it dim commands without handlers for terminal
  * @returns The graph
  */
-export function treeGraph(command: Command): string {
-    const graph = recursiveTree(command, 0);
+export function treeGraph(command: Command, colored: boolean = false): string {
+    const graph = recursiveTree(command, 0, colored);
     return command.name + '\n' + graph;
 }
