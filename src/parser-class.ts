53a035eea0b7f4d99523c5380031b4ef53004753
rdsq <62446963+rdsq@users.noreply.github.com>
2024-06-19 19:20:19 +0300

    parser class ok?

--- src/parser-class.ts
+++ src/parser-class.ts
@@ -1,5 +1,7 @@
+import assert from "assert";
 import { exitWithErrorInternal } from "./exit-with-error-internal.js";
 import { Option, Command, Arg, YaclilOptions, ParsedCommand, ParsedOption, ParsedOptions } from "./types.js";
+import { ParsedArgs } from "./types/parsed.js";
 
 function checkIsHelp(option: string) {
     return option === '--help' || option === '-h';
@@ -12,15 +14,38 @@ function isNumeric(value: string) {
     return /^-?\d+$/.test(value);
 }
 
+function getArgName(arg: Arg | string) {
+    if (typeof arg === 'string') {
+        return arg;
+    } else {
+        return arg.name;
+    }
+}
+
+function isArgRequired(arg: Arg | string): boolean {
+    if (typeof arg === 'string') {
+        return false;
+    } else {
+        return arg.required ?? false;
+    }
+}
+
+function isOption(element: string) {
+    return element.startsWith('-') && !isNumeric(element);
+}
+
 export default class Parser {
     rootCommand: Command;
     initOptions: YaclilOptions;
     argv: string[];
 
     currentCommand: Command;
+    currentOption: ParsedOption | null;
     commandSearchStopped: boolean;
     options: ParsedOptions;
-    args: Arg[];
+    args: string[];
+    argsForCurrentOption: string[];
+    processedArgsForCurrentCommand: ParsedArgs;
     unexpectedOptions: string[];
     unexpectedOptionsShort: string[];
     unexpectedArgs: string[];
@@ -42,9 +67,12 @@ export default class Parser {
         this.treePath = [ options.rootCommandName ];
 
         this.currentCommand = this.rootCommand;
+        this.currentOption = null;
         this.commandSearchStopped = false;
         this.options = {};
         this.args = [];
+        this.argsForCurrentOption = [];
+        this.processedArgsForCurrentCommand = {};
         this.unexpectedOptions = [];
         this.unexpectedOptionsShort = [];
         this.unexpectedArgs = [];
@@ -75,11 +103,6 @@ export default class Parser {
     }
 
     processOption(option: string): void {
-        // if options are disabled or this option is just a negative number
-        if (!this.optionsProcessingEnabled || isNumeric(option)) {
-            // process it as an argument
-            return;
-        };
         // check if this is help
         if (checkIsHelp(option)) {
             this.helpCalled = true;
@@ -126,7 +149,116 @@ export default class Parser {
         }
     }
 
-    parse(): void {}
+    isItACommand(element: string): boolean {
+        // if the search has been stopped
+        if (this.commandSearchStopped) return false;
+        // if it is in subcommands
+        if (element in (this.currentCommand.subcommands ?? {})) return true;
+        // or no
+        return false;
+    }
+
+    processCommand(arg: string) {
+        assert(this.currentCommand.subcommands, `Subcommand "${arg}" does not exist`)
+        this.currentCommand = this.currentCommand.subcommands[arg];
+    }
+
+    processArg(arg: string) {
+        if (this.currentOptionExpectsArgs > 0) {
+            // args for options
+            this.argsForCurrentOption.push(arg);
+            this.currentOptionExpectsArgs--;
+            if (this.currentOptionExpectsArgs === 0) {
+                // if all args were provided
+                this.setArgsForOption();
+            }
+            return;
+        }
+        if (this.currentCommandExpectsArgs > 0) {
+            // args for commands
+            this.args.push(arg);
+            this.currentCommandExpectsArgs--;
+            return;
+        }
+        if (this.unexpectedArgsAllowed) {
+            // if unexpected args are allowed
+            this.unexpectedArgs.push(arg);
+        }
+        // else print error
+        exitWithErrorInternal(
+            `Error: unexpected arg "${arg}"`,
+            this.treePath,
+        )
+    }
+
+    setArgsForOption() {
+        assert(this.currentOption !== null, 'Current option is not defined');
+        const option = this.currentOption;
+        if (option.option.args) {
+            // if args are defined
+            for (let i = 0; i < option.option.args.length; i++) {
+                const arg = option.option.args[i];
+                const argName = getArgName(arg);
+                const providedArg = this.argsForCurrentOption[i];
+                if (!providedArg) {
+                    if (isArgRequired(arg)) {
+                        // if arg was not provided and it is required
+                        exitWithErrorInternal(
+                            `Error: required argument "${argName}" was not provided`,
+                            this.treePath,
+                        );
+                    }
+                }
+                this.currentOption.args[argName] = providedArg;
+            }
+        }
+        this.argsForCurrentOption = []; // clean it
+    }
+
+    setArgsForCommand() {
+        const command = this.currentCommand;
+        const args: ParsedArgs = {};
+        if (command.args) {
+            for (let i = 0; i < command.args.length; i++) {
+                const arg = command.args[i];
+                const argName = getArgName(arg);
+                const providedArg = this.args[i];
+                if (!providedArg) {
+                    if (isArgRequired(arg)) {
+                        exitWithErrorInternal(
+                            `Error: required argument "${argName}" was not provided`,
+                            this.treePath,
+                        );
+                    }
+                }
+                args[argName] = providedArg;
+            }
+        }
+        this.processedArgsForCurrentCommand = args;
+    }
+
+    parse(): void {
+        for (const arg of this.argv) {
+            if (this.optionsProcessingEnabled) {
+                this.processOption(arg);
+                continue;
+            }
+            if (isOption(arg)) {
+                this.setArgsForOption(); // set args for previous option
+                this.processOption(arg);
+                continue;
+            }
+            if (!this.commandSearchStopped && arg in (this.currentCommand.subcommands ?? {})) {
+                this.setArgsForOption(); // set args for previous option
+                this.processCommand(arg);
+                continue;
+            }
+            // else
+            this.processArg(arg);
+        }
+        this.setArgsForOption();
+        this.setArgsForCommand();
+    }
 
     get parsedObject(): ParsedCommand {
         return {
@@ -138,6 +270,7 @@ export default class Parser {
             options: this.options,
             helpOption: this.helpCalled,
             unexpectedOptionsShort: this.unexpectedOptionsShort,
+            args: this.processedArgsForCurrentCommand,
         }
     }
 }
--- src/types/parsed.ts
+++ src/types/parsed.ts
@@ -3,17 +3,19 @@ import type { Option } from "./option";
 
 export type ParsedOption = {
     option: Option,
-    args: { [key: string]: string },
+    args: ParsedArgs,
 };
 
 export type ParsedOptions = { [key: string]: ParsedOption };
+
+export type ParsedArgs = { [key: string]: string };
 
 export type ParsedCommand = {
     command: Command,
     options: ParsedOptions,
     unexpectedOptions: string[],
     unexpectedOptionsShort: string[],
-    args: { [key: string]: string },
+    args: ParsedArgs,
     unexpectedArgs: string[],
     treePath: string[],
     helpOption: boolean,
