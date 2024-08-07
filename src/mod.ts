e43c0669c04f36db5fec08a7838a94170d41a4e0
rdsq <62446963+rdsq@users.noreply.github.com>
2024-08-07 13:52:45 +0300

    exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,1 +1,11 @@
-export { yaclil } from "./yaclil.ts";
+import { yaclil } from "./yaclil.ts";
+import * as util from './util.ts';
+import * as std from './std.ts';
+import type * as types from '../types.d.ts';
+
+export {
+    yaclil,
+    util,
+    std,
+    type types,
+}
