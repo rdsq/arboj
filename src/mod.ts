e6c9d72b587af2199f0f87b61e7e339312ea164b
rdsq <62446963+rdsq@users.noreply.github.com>
2024-08-15 16:30:42 +0300

    docs for modules

--- src/mod.ts
+++ src/mod.ts
@@ -3,4 +3,12 @@ import * as util from './util.ts';
 import * as std from './std.ts';
 import type * as types from '../types.d.ts';
 
-export { arboj, std, type types, util };
+export {
+    arboj,
+    /** Standard commands and options from Arboj */
+    std,
+    /** Arboj types */
+    type types,
+    /** Arboj utilities, can be used with or without Arboj, depending on the util */
+    util
+};
